6c81355bd25cf4e4cec742489aa118ffd1a8d5ff
amit <amit@gmail.com>
2025-06-14 02:46:34 +0530

    c++ basics

---
+++ 1_basic/first_program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+
+/*
+    #include => preprocessor directive
+    iostream contains definition of cout/cin
+    std namespace contains the declaration
+*/
+
+int main(){
+
+    cout << "Hello World" << endl;
+    cout << PI << endl;
+    int a$b = 100;
+    cout << a$b << endl;
+    return 0;
+}
